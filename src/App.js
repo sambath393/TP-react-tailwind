ef6f21d927d087c0462e4475f46c408a0f9b2bea
Sambath <57914899+sambath393@users.noreply.github.com>
2022-08-07 21:58:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
+        <p className='text-7xl'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: { ...colors },
+      height: {
+        inherit: 'inherit',
+      },
+      width: {
+        inherit: 'inherit',
+      },
+    },
+  },
+  plugins: [],
+};
+
